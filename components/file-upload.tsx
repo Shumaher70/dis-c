32322e81d919d45fea10278232a82379626fa399
oleg <olegvraby@gmail.com>
2024-05-15 18:51:06 +0200

    create:file-upload.tsx: FileUpload was created using logic for images for the initial mode

---
+++ components/file-upload.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+}
+
+const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileType = value?.split(".").pop();
+
+  if (value && fileType !== "pdf") {
+    return (
+      <div className="relative h-20 w-20 ">
+        <Image
+          fill
+          src={value}
+          alt="Upload"
+          className="rounded-full object-cover"
+        />
+
+        <button
+          onClick={() => onChange("")}
+          className="absolute right-0 top-0 rounded-full bg-rose-500 p-1 text-white shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
+
+export default FileUpload;
