a6570eba264c33d444d539c01a9e4d7da63a4dc9
oleg <olegvraby@gmail.com>
2024-05-16 15:01:59 +0200

    create: (main)/(routes)/servers/[severId]/layout.ts: 01.The layout has been created 02.The profiler checker has been added 03.The server checker has been added 04.The markup has been created and the ServerSidebar component has been rendered

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,49 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { prisma } from "@/lib/prisma";
+import { currentProfile } from "@/lib/current-profile";
+
+import ServerSidebar from "@/components/server/server-sidebar";
+
+const ServerIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: {
+    serverId: string;
+  };
+}) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  const server = await prisma.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="h-full">
+      <div className="fixed inset-y-0 z-20 hidden h-full w-60 flex-col md:flex">
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
+};
+
+export default ServerIdLayout;
