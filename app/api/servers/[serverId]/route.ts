c699df3ea7594cbdff6796222a5eabe3ec18ccc5
oleg <olegvraby@gmail.com>
2024-05-20 19:34:29 +0200

    add: api/servers/[serverId]/route.ts: The logic for delete server has been added

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,6 +2,31 @@ import { currentProfile } from "@/lib/current-profile";
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
+export const DELETE = async (
+  req: Request,
+  { params }: { params: { serverId: string } },
+) => {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const server = await prisma.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
+
 export const PATCH = async (
   req: Request,
   { params }: { params: { serverId: string } },
