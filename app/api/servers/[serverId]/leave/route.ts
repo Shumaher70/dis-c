8c32cb6a6ce66d99b8bfe70115be75269dd6c5e5
oleg <olegvraby@gmail.com>
2024-05-20 18:58:11 +0200

    create:api/servers/[serverId]/leave/route.ts: the logic for updating server has been added

---
+++ app/api/servers/[serverId]/leave/route.ts
@@ -0,0 +1,54 @@
+import { currentProfile } from "@/lib/current-profile";
+import { NextResponse } from "next/server";
+
+import { prisma } from "@/lib/prisma";
+
+export const PATCH = async (
+  req: Request,
+  {
+    params,
+  }: {
+    params: {
+      serverId: string;
+    };
+  },
+) => {
+  try {
+    const profile = await currentProfile();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    const server = await prisma.server.update({
+      where: {
+        id: params.serverId,
+        profileId: {
+          not: profile.id,
+        },
+        members: {
+          some: {
+            profileId: profile.id,
+          },
+        },
+      },
+
+      data: {
+        members: {
+          deleteMany: {
+            profileId: profile.id,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_LEAVE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+};
