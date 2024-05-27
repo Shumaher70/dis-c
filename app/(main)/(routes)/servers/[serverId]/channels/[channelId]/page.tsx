8dcf88f6085f6f017d8dbb6b70b55901be67729a
Sergey19972 <gusak50gusak@yandex.ru>
2024-05-27 16:58:39 +0200

    Feathure/dis 12 create a chat header (#17)
    * add:ChannelIdPage: 01.Mark up 02.Checker to the initial profile and channelExists
    
    * create:mobile-toggle: logic
    
    * create:chat-header.tsx: Mark up
    
    * create:sheet: has been downloaded from "npx shadcn-ui@latest add sheet"
    
    ---------
    
    Co-authored-by: Oleg Vrabiy <olegvraby@gmail.com>

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const ChannelIdPage = () => {
-  return <div>ChannelIdPage</div>;
+import { currentProfile } from "@/lib/current-profile";
+import { prisma } from "@/lib/prisma";
+import ChatHeader from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return auth().redirectToSignIn();
+  }
+
+  const channel = await prisma.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await prisma.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if (!channel || !member) {
+    redirect("/");
+  }
+
+  return (
+    <div className="flex h-full flex-col bg-white dark:bg-[#313338]">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
