5ca0662d4e05c2282d99aea360ad278a343c4a63
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-03 22:00:43 +0200

    fix:media-room.tsx: remove unused import

--- components/media-room.tsx
+++ components/media-room.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 import { LiveKitRoom, VideoConference } from "@livekit/components-react";
 import "@livekit/components-styles";
-import { Channel } from "@prisma/client";
 import { useUser } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 
